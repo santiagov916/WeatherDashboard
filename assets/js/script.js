5056b21897425be1b287de405f6f9f7fa09b9656
Santiago Venegas <santiagovenegas@Santiagos-MacBook-Pro.local>
2022-03-22 02:20:13 -0700

    date for each card is working / card loads with data entry / no errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
 // Global var
-var apiRoot = 'http://api.openweathermap.org';
+var apiRoot = 'https://api.openweathermap.org';
 var apiRootWeather = 'https://api.openweathermap.org';
 var apiKey = 'b0af02ce6d6578e341aee9bf7fa71ce7';
 
-var fiveDayForecast = document.querySelector('#five-day-forecast');
-var forecastCardContainer = document.querySelector('#display-each-card');
+var fiveDayContainter = document.querySelector('#display-each-card');
 var todaysForecastContainer = document.querySelector('#todays-forecast');
+var cityTitle = document.querySelector('#city-title');
+
+dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
 
 
 // function for when we search for a city
@@ -21,23 +24,21 @@ function dispalyTodaysWeather() {
 
 }
 
-function forCreatingForecastCard(city, weather) {
-    // response data
-    var tempF = weather.temp;
-    var wind = weather.wind_speed;
-    var humidity = weather.humidity;
+function forCreatingForecastCard(forecast, timezone) {
 
-    console.log(tempF);
-    console.log(wind);
-    console.log(humidity);
+    var unixTs = forecast.dt;
 
-    console.log(city);
+    var tempMin = forecast.temp.min;
+    var tempMax = forecast.temp.max;
+    var wind = forecast.wind_speed;
+    var humidity = forecast.humidity;
 
-    var cardContainer = document.createElement('div');
-    cardContainer.setAttribute('class', 'col-4 p-1');
+
+    var cardPlacer = document.createElement('div');
+    cardPlacer.setAttribute('class', 'col-4 p-1');
 
     var actualCard = document.createElement('div');
-    actualCard.setAttribute('class', 'm-2 w-100');
+    actualCard.setAttribute('class', 'card m-2 w-100');
 
     var cardImg = document.createElement('span');
     cardImg.setAttribute('class', 'card-img-rainy');
@@ -48,40 +49,62 @@ function forCreatingForecastCard(city, weather) {
     var cardHeader = document.createElement('h3');
     cardHeader.setAttribute('class', 'card-title text-center border-bottom');
 
-    var temperature = document.createElement('p');
-    temperature.setAttribute('class', 'p-2 text-left');
-    temperature.textContent = `Temp: ${tempF}`;
+    var temperatureMin = document.createElement('p');
+    temperatureMin.setAttribute('class', 'card-text p-2 text-left');
+    temperatureMin.textContent = `Minimum Temp: ${tempMin}`;
+
+    var temperatureMax = document.createElement('p');
+    temperatureMax.setAttribute('class', 'card-text p-2 text-left');
+    temperatureMax.textContent = `Maximum Temp: ${tempMax}`;
 
     var windSpace = document.createElement('p');
-    windSpace.setAttribute('class', 'p-2 text-left');
+    windSpace.setAttribute('class', 'card-text p-2 text-left');
     windSpace.textContent = `Wind speed: ${wind} mph`;
 
     var humiditySpace = document.createElement('p');
-    humiditySpace.setAttribute('class', 'p-2 text-left');
+    humiditySpace.setAttribute('class', 'card-text p-2 text-left');
     humiditySpace.textContent = `Humidity: ${humidity} %`;
 
-    cardBody.append(cardHeader, temperature, windSpace, humiditySpace);
+    var dateOf = document.createElement('h5');
+    dateOf.setAttribute('class', 'card-title text-primary');
+    dateOf.textContent =dayjs.unix(unixTs).tz(timezone).format('M/D/YYYY');
+
+    cardBody.append(dateOf, temperatureMin, temperatureMax, windSpace, humiditySpace);
     actualCard.append(cardImg, cardBody);
-    cardContainer.append(actualCard);
-    forecastCardContainer.append(cardContainer);
+    cardPlacer.append(actualCard);
+    fiveDayContainter.append(cardPlacer);
+
+
+
 }
 
-function displayFiveDayForecast(city, dailyForecast) {
+function displayFiveDayForecast(dailyForecast, timezone) {
+
+    var startDt = dayjs().tz(timezone).add(1, 'day').startOf('day').unix();
+    var endDt = dayjs().tz(timezone).add(6, 'day').startOf('day').unix();
 
     var cityEl = document.createElement('h3');
-    cityEl.textContent = `5-day forecast for: ${city}`;
+    cityEl.textContent = `5-day forecast:`;
     var headerDiv = document.createElement('div');
     headerDiv.setAttribute('class', 'col-12 mr-auto show-city-name border-bottom');
 
     headerDiv.append(cityEl);
-    fiveDayForecast.append(headerDiv);
+    fiveDayContainter.append(headerDiv);
 
-    forCreatingForecastCard(dailyForecast);
+
+    for (var int = 0; int < dailyForecast.length; int++) {
+
+        if (dailyForecast[int].dt >= startDt && dailyForecast[int].dt < endDt) {
+
+            forCreatingForecastCard(dailyForecast[int], timezone);
+        }
+    }
+
 }
 
 function displayItems(city, data) {
-    dispalyTodaysWeather(city, data.current, data);
-    displayFiveDayForecast(city, data.daily, data);
+    dispalyTodaysWeather(city, data.current, data.timezone);
+    displayFiveDayForecast(data.daily, data.timezone);
 }
 
 // function for api request
@@ -94,36 +117,33 @@ function getApiInfo(location) {
     var apiForWeather = `${apiRootWeather}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly,alerts&appid=${apiKey}`;
 
     fetch(apiForWeather)
-    .then(function (res) {
-        return res.json();
+    .then(function(response) {
+
+        if (response.ok) {
+            response.json().then(function(data) {
+                cityTitle.textContent = `${city}`;
+
+                displayItems(city, data);
+            })
+        }
     })
-    .then(function (data) {
-        displayItems(city, data)
-    })
-    .catch(function (err) {
-        console.log(err)
-    });
 }
 
 function apiGeoCode(search) {
 
-    var apiUrl = `${apiRoot}/geo/1.0/direct?q=${search}&limit=5&appid=${apiKey}`;
+    var apiUrl = `${apiRoot}/geo/1.0/direct?q=${search}&limit=1&appid=${apiKey}`;
 
-    fetch(apiUrl)
-    .then( function(res) {
-        console.log(res)
-        return res.json();
-    })
-    .then(function (data) {
-        if (!data[0]) {
-            alert("Location doesn't exist!");
-        } else {
-            getApiInfo(data[0]);
-        }
-    })
-    .catch(function (err) {
-        console.log(err);
-    });
+   fetch(apiUrl)
+   .then(function(response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function(data) {
+        getApiInfo(data[0]);
+      });
+    } else {
+      alert('Error: Location doesn`t exist!');
+    }
+  })
 
 }
 
